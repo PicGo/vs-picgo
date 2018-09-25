743800080396267d4599f94a93ef90c533335faa
Spades-S <allengefeng@163.com>
2018-09-25 18:28:23 +0800

    Optimize code sturcture

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # vs-picgo(Vscode Plugin for PicGo)
 
-> [PicGo](https://github.com/PicGo)的vscode插件
+> [PicGo](https://github.com/PicGo)的vscode插件
 
 ## 功能
-利用vs-picgo可以直接将剪切板中的图片插入到markdown文件中，可极大提升markdown的编写效率
+利用vs-picgo可以直接将剪切板中的图片插入到markdown文件中，可极大提升markdown的编写效率
 ## 配置
 
 * 0配置：采用默认配置，默认配置中图床采用SM.MS
 * 自定义配置：
-在vscode配置信息文件usersetting.json中加入
+在vscode配置信息文件usersetting.json中加入
 ``` json
 {
 	"picgo": {
@@ -37,7 +37,7 @@
 		"area": "",
 		"options": ""
 	},
-	"upyun": {          // 又拍云图床配置
+	"upyun": {          // 又拍云图床配置
 		"bucket": "",
 		"operator": "",
 		"password": "",
@@ -63,7 +63,7 @@
 		"branch": "",
 		"username": ""
 	},
-	"aliyun": {       // 阿里云图床配置
+	"aliyun": {       // 阿里云图床配置
 		"accessKeyId": "",
 		"accessKeySecret": "",
 		"bucket": "",
@@ -77,10 +77,10 @@
 
 
 ## 使用
-* 在插件商店中查找**picgo**，并安装
-* 安装完成后，可在命令面板中输入PicGo运行插件
+* 在插件商店中查找**picgo**，并安装
+* 安装完成后，可在命令面板中输入PicGo运行插件
 * 推荐：为PicGo设置键盘快捷方式（自行设定）
-
+
 
 ## Thanks
 [PicGo-Core](https://github.com/PicGo/PicGo-Core)
--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,13 @@
 'use strict';
 import * as vscode from 'vscode';
-import { spawn } from 'child_process';
+import { spawn, ChildProcess } from 'child_process';
 import * as  path from 'path';
 import * as fs from 'fs';
 
 const PicGo = require('picgo');
 
 export function activate(context: vscode.ExtensionContext) {
-
-    let disposable = vscode.commands.registerCommand('extension.picgo', () => {
-        start();
-    });
-
+    const disposable = vscode.commands.registerCommand('extension.picgo', () => start());
     context.subscriptions.push(disposable);
 }
 
@@ -21,90 +17,88 @@ export function deactivate() {
 function start(): void {
     const editor = vscode.window.activeTextEditor;
     // 当前是否有active编辑器
-    if (editor) {
-        // 插件只支持markdown文法
-        if (editor.document.languageId === 'markdown') {
-            const imageDir = path.join(editor.document.uri.fsPath, '../.imgs/');
-            try {
-                fs.accessSync(imageDir);
-            } catch (err) {
-                fs.mkdirSync(imageDir);
-            }
-            const imageName = getImageName(editor);
-            saveImageFromClipboardandGetPath(`${imageDir}/${imageName}.png`)
-                .then(path => {
-                    if (path === 'no image') {
-                        vscode.window.showErrorMessage('copy image first');
-                    } else {
-                        const picgoConfig = vscode.workspace.getConfiguration('picgo');
-                        const picgo = new PicGo(picgoConfig.path || '');
-                        picgo.upload([path]);
-                        picgo.on('finished', (ctx: any) => {
-                            editor.edit(textEditor => {
-                                textEditor.replace(editor.selection, `![image ${imageName}](${ctx.output[0].imgUrl})`);
-                                vscode.window.showInformationMessage('upload successfully');
-                            });
-                        });
-                        picgo.on('notification', (notice: any) => {
-                            vscode.window.showErrorMessage(notice.title);
-                        });
-                    }
-                })
-                .catch(() => {
-                });
+    // 插件只支持markdown文法
+    if (editor && editor.document.languageId === 'markdown') {
+        const imageDir = path.join(editor.document.uri.fsPath, '../.imgs');
+        try {
+            fs.accessSync(imageDir);
+        } catch (err) {
+            fs.mkdirSync(imageDir);
         }
+        const imageName = getImageName(editor);
+        getImagePath(saveImageFromClipboard(`${imageDir}/${imageName}.png`))
+            .then(path => {
+
+                if (path === 'no image') {
+                    vscode.window.showErrorMessage('copy image first');
+                } else {
+                    const picgoConfig = vscode.workspace.getConfiguration('picgo');
+                    const picgo = new PicGo(picgoConfig.path || '');
+                    picgo.upload([path]);
+                    picgo.on('finished', (ctx: any) => {
+                        editor.edit(textEditor => {
+                            textEditor.replace(editor.selection, `![image ${imageName}](${ctx.output[0].imgUrl})`);
+                            vscode.window.showInformationMessage('upload successfully');
+                        });
+                    });
+                    picgo.on('notification', (notice: any) => {
+                        vscode.window.showErrorMessage(notice.title);
+                    });
+                }
+            })
+            .catch(() => {
+            });
     } else {
         vscode.window.showErrorMessage('No Active Editor!');
     }
 }
 
-function getImageName(editor: vscode.TextEditor): String {
+function getImageName(editor: vscode.TextEditor): string {
     const selectedString = editor.document.getText(editor.selection);
-    // 补充完全命名限制
-    if (selectedString && /[^:.\/\?\$]+$/.test(selectedString)) {
-        return `${selectedString}`;
-    }
-    return `${new Date().getTime()}`;
+    const nameReg = /[^:.\/\?\$]+$/;  // 补充完全命名限制
+    return (selectedString && nameReg.test(selectedString)) ? selectedString : `${new Date().getTime()}`;
 }
 
 
-function saveImageFromClipboardandGetPath(imagePath: string): Promise<String> {
-    return new Promise((resolve, reject) => {
-        let platform: string = process.platform;
-        let execution = null;
-        const platformPaths: {
-            [index: string]: string
-        } = {
-            'darwin': '../lib/mac.applescript',
-            'win32': '../lib/windows.ps1',
-            'linux': '../lib/linux.sh'
-        };
-        const scriptPath = path.join(__dirname, platformPaths[platform]);
-        if (platform === 'darwin') {
-            execution = spawn('osascript', [scriptPath, imagePath]);
-
-        } else if (platform === 'win32') {
-            execution = spawn('powershell', [
-                '-noprofile',
-                '-noninteractive',
-                '-nologo',
-                '-sta',
-                '-executionpolicy', 'unrestricted',
-                '-windowstyle', 'hidden',
-                '-file', scriptPath,
-                imagePath
-            ]);
-        } else {
-            execution = spawn('sh', [scriptPath, imagePath]);
+function saveImageFromClipboard(imagePath: string): ChildProcess {
+    const platform: string = process.platform;
+    const platformMaps: {
+        [index: string]: {
+            script: string,
+            path: string
         }
+    } = {
+        'darwin': {
+            script: 'osascript',
+            path: '../lib/mac.applescript'
+        },
+        'win32': {
+            script: 'powershell',
+            path: '../lib/windows.ps1'
+        },
+        'linux': {
+            script: 'sh',
+            path: '../lib/linux.sh'
+        }
+    };
+    const args = platform === 'win32' ? [
+        '-noprofile',
+        '-noninteractive',
+        '-nologo',
+        '-sta',
+        '-executionpolicy', 'unrestricted',
+        '-windowstyle', 'hidden',
+        '-file'] : [];
 
-        execution.stdout.on('data', (data: Buffer) => {
-            if (platform === 'linux') {
-                if (data.toString().trim() === 'no xclip') {
-                    vscode.window.showErrorMessage('Please install xclip before run picgo!');
-                } else {
-                    resolve(data.toString().trim());
-                }
+    args.push(path.join(__dirname, platformMaps[platform].path), imagePath);
+    return spawn(platformMaps[platform].script, args);
+}
+
+function getImagePath(execution: ChildProcess): Promise<string> {
+    return new Promise((resolve, reject) => {
+        execution.stdout.on('data', (data: string) => {
+            if (process.platform === 'linux' && data.toString().trim() === 'no xclip') {
+                vscode.window.showErrorMessage('Please install xclip before run picgo!');
             } else {
                 resolve(data.toString().trim());
             }
@@ -114,6 +108,7 @@ function saveImageFromClipboardandGetPath(imagePath: string): Promise<String> {
         });
     });
 }
+
 
 
 
